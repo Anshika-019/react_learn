e6f90fc950d5ffce692e579de841a21303bb3015
AnshikaPandey-019 <anshika.pandey@tetruscorp.com>
2024-05-07 16:48:35 +0530

    Create your own react library and JSX.

--- basicvite/src/App.jsx
+++ basicvite/src/App.jsx
@@ -1,12 +1,10 @@
-import Youtube from "./youtube"
+import Youtube from "./Youtube";
 
 function App() {
-
-
   return (
     <>  
       <h1> Vite react app</h1>
-      <h1> Vite react app</h1>
+      <h1> Vite react apps</h1>
       <Youtube/>
     </>
   )
--- basicvite/src/main.jsx
+++ basicvite/src/main.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+  type: 'a',
+  props:{
+    href: "https://google.com",
+    target: "_blank",
+},
+children: "Click me to visit google"
+}
+
+function MyApp() {
+  return <div>Custom React App</div>
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+   <MyApp />
 )
